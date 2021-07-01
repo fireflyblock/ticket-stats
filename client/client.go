2f060a6027c23784ca60d92a3c398e66bd58583c
urugang <urugang@gmail.com>
2021-07-01 21:29:00 +0800

    upgrade to v1.10.0

--- client/client.go
+++ client/client.go
@@ -3,37 +3,37 @@ package client
 import (
 	"context"
 	"fmt"
-	"github.com/filecoin-project/lotus/api"
-	lotusClient "github.com/filecoin-project/lotus/api/client"
 	"net/http"
 	"os"
+
+	"github.com/filecoin-project/lotus/api"
+	lotusClient "github.com/filecoin-project/lotus/api/client"
 )
 
 var Client api.FullNode
 var SignClient api.FullNode
 
-
 func CreateLotusClient() error {
-	host:=os.Getenv("LOTUS_HOST")
+	host := os.Getenv("LOTUS_HOST")
 	var err error
 	requestHeader := http.Header{}
 	requestHeader.Add("Content-Type", "application/json")
-	Client, _, err = lotusClient.NewFullNodeRPC(context.Background(), host, requestHeader)
+	Client, _, err = lotusClient.NewFullNodeRPCV1(context.Background(), host, requestHeader)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 func CreateLotusSignClient() error {
-	host:=os.Getenv("LOTUS_HOST")
-	token:=os.Getenv("LOTUS_SIGN_TOKEN")
+	host := os.Getenv("LOTUS_HOST")
+	token := os.Getenv("LOTUS_SIGN_TOKEN")
 
 	var err error
 	requestHeader := http.Header{}
 	requestHeader.Add("Content-Type", "application/json")
 	tokenHeader := fmt.Sprintf("Bearer %s", token)
 	requestHeader.Set("Authorization", tokenHeader)
-	SignClient, _, err = lotusClient.NewFullNodeRPC(context.Background(), host, requestHeader)
+	SignClient, _, err = lotusClient.NewFullNodeRPCV1(context.Background(), host, requestHeader)
 	if err != nil {
 		return err
 	}
--- go.mod
+++ go.mod
@@ -4,10 +4,11 @@ go 1.15
 
 require (
 	github.com/filecoin-project/go-address v0.0.5
-	github.com/filecoin-project/go-state-types v0.1.0
-	github.com/filecoin-project/lotus v1.8.0
+	github.com/filecoin-project/go-state-types v0.1.1-0.20210506134452-99b279731c48
+	github.com/filecoin-project/lotus v1.10.0
 	github.com/ipfs/go-log/v2 v2.1.3
-	github.com/urfave/cli/v2 v2.3.0
+	github.com/urfave/cli/v2 v2.2.0
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
 )
+
 replace github.com/filecoin-project/filecoin-ffi => ./extern/filecoin-ffi
